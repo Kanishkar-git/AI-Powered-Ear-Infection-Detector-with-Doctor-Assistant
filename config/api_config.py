47c5eb1d3dd93fa1cd3c5c78d438782b6dd6e479
Kanishkar <kanishkar427@gmail.com>
2025-12-31 20:44:37 +0530

    Update api_config.py

--- config/api_config.py
+++ config/api_config.py
@@ -1,17 +1,13 @@
 import streamlit as st
 from inference_sdk import InferenceHTTPClient
-from google import genai
+import google.generativeai as genai
 
-
-# ---------------- Roboflow ----------------
 
 def get_roboflow_client():
     try:
         api_key = st.secrets["ROBOFLOW_API_KEY"]
     except KeyError:
-        raise RuntimeError(
-            "ROBOFLOW_API_KEY not found. Please add it to .streamlit/secrets.toml"
-        )
+        raise RuntimeError("ROBOFLOW_API_KEY not found in Streamlit secrets")
 
     return InferenceHTTPClient(
         api_url="https://serverless.roboflow.com",
@@ -19,17 +15,14 @@ def get_roboflow_client():
     )
 
 
-# ---------------- Gemini Client ----------------
-
-def get_gemini_client():
-    api_key = get_gemini_api_key()
-    return genai.Client(api_key=api_key)
-
-
 def get_gemini_api_key():
     try:
         return st.secrets["GEMINI_API_KEY"]
     except KeyError:
-        raise RuntimeError(
-            "GEMINI_API_KEY not found. Please add it to .streamlit/secrets.toml"
-        )
+        raise RuntimeError("GEMINI_API_KEY not found in Streamlit secrets")
+
+
+def get_gemini_client():
+    api_key = get_gemini_api_key()
+    genai.configure(api_key=api_key)
+    return genai
